e5ac1a27b0a4e275bac67cd17ac3947419fb8d92
Andrei Dayrit <22058759@saultcollege.ca>
2023-09-27 14:22:36 -0400

    changes app.java

--- lab1/src/lab1/q1/App.java
+++ lab1/src/lab1/q1/App.java
@@ -1,4 +1,9 @@
 package lab1.q1;
 
 public class App {
+
+
+    public static void main(String[] args) {
+
+    }
 }
--- lab1/src/lab1/q2/App.java
+++ lab1/src/lab1/q2/App.java
@@ -1,4 +1,9 @@
 package lab1.q2;
 
 public class App {
+
+
+    public static void main(String[] args) {
+
+    }
 }
