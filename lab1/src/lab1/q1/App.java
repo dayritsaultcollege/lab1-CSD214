ed1b758c789bd668f61fd12f4832f8b40c15c088
Andrei Dayrit <22058759@saultcollege.ca>
2023-09-28 12:54:59 -0400

    Changes

--- lab1/src/lab1/q1/App.java
+++ lab1/src/lab1/q1/App.java
@@ -1,16 +1,37 @@
 package lab1.q1;
 
-public class App extends Main{
+
+/**
+ Author : Andrei Dayrit
+ Date : 09/27/2023
+ Description : declare the variables and create a constructor.
+ **/
+
+public class App {
 
 
-    public App(String firstname, String lastname, String gender, int SIN, int COUNT) {
-        super(firstname, lastname, gender, SIN, COUNT);
+    //global variables declaration
+
+    String firstname, lastname;
+    String gender;
+    int SIN;
+    int COUNT;
+
+
+
+
+    public App(String firstname, String lastname, String gender, int SIN, int COUNT){
+
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.gender = gender;
+        this.SIN = SIN;
+        this.COUNT = COUNT;
+
+        
+
     }
 
-    public static void main(String[] args) {
 
 
-
-
-    }
 }
--- lab1/src/lab1/q1/Main.java
+++ lab1/src/lab1/q1/Main.java
@@ -1,34 +1,31 @@
 package lab1.q1;
 
-
 /**
  Author : Andrei Dayrit
  Date : 09/27/2023
- Description : describe what your program does.
+ Description : get the values of the object and print it.
  **/
-
-public class Main {
-
-
-    //global variables declaration
-
-    String firstname, lastname;
-    String gender;
-    int SIN;
-    int COUNT;
+public class Main extends App {
 
 
-
-    public Main(String firstname, String lastname, String gender, int SIN, int COUNT){
-
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.gender = gender;
-        this.SIN = SIN;
-        this.COUNT = COUNT;
-
+    //constructor of the class
+    public Main(String firstname, String lastname, String gender, int SIN, int COUNT) {
+        super(firstname, lastname, gender, SIN, COUNT);
     }
 
+    public static void main(String[] args) {
+
+        //create an object of the class
+        Main person = new Main("Andrei","Dayrit","M",1234,1);
+        Main person2 = new Main("Fredrich","Dayrit","M",5678,2);
 
 
+        //print the values of the object
+        System.out.println(person.firstname +" "+ person.lastname+", "+ person.gender +", SIN="+ person.SIN);
+        System.out.println(person2.firstname +" "+ person2.lastname+", "+ person2.gender +", SIN="+ person2.SIN);
+
+
+
+
+    }
 }
