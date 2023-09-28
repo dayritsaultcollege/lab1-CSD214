f0ce2a0447dd6b06f1ee6881ac1a0950bb783a4d
Andrei Dayrit <22058759@saultcollege.ca>
2023-09-28 14:14:40 -0400

    Updates in Car Class

--- lab1/src/lab1/q1/Main.java
+++ lab1/src/lab1/q1/Main.java
@@ -5,7 +5,7 @@ package lab1.q1;
  Date : 09/27/2023
  Description : get the values of the object and print it.
  **/
-public class Main extends App {
+public class Main extends App{
 
 
     //constructor of the class
@@ -13,16 +13,21 @@ public class Main extends App {
         super(firstname, lastname, gender, SIN, COUNT);
     }
 
+    public Main() {
+    }
+
     public static void main(String[] args) {
 
         //create an object of the class and pass the values
         Main p1 = new Main("Andrei","Dayrit", "M",1234,1);
-        Main p2 = new Main("Fredrich","Dayrit","M",5678,2);
+        Main p2 = new Main("Handy","Man","M",5678,2);
+        Main p3 = new Main("Aneglica","Mori","F",9101,3);
 
 
         //print the values of the object
         System.out.println(p1.firstname +" "+ p1.lastname+", "+ p1.gender +", SIN="+ p1.SIN);
         System.out.println(p2.firstname +" "+ p2.lastname+", "+ p2.gender +", SIN="+ p2.SIN);
+        System.out.println(p3.firstname +" "+ p3.lastname+", "+ p3.gender +", SIN="+ p3.SIN);
 
 
 
